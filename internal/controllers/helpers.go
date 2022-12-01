44e56d2eb3134da333c210b281b4d13105cbc494
Allyson-English <allyson.english@gmail.com>
2022-12-01 16:08:24 -0500

    hard code date range

--- internal/controllers/helpers.go
+++ internal/controllers/helpers.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"time"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
 )
@@ -19,27 +17,12 @@ func ValidateQueryParams(p *QueryValues, c *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
-	p.RangeStart, p.RangeEnd, err = validateDateParams(p.RangeStart, p.RangeEnd)
+	p.RangeStart = "2022-01-01"
+	p.RangeEnd = "now"
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-func validateDateParams(start, end string) (string, string, error) {
-	// defaults to past 24 horus if no time range is specified
-	if start == "" || end == "" {
-		return "2022-01-01", "now", nil
-	}
-	sd, err := time.Parse("20060102", start)
-	if err != nil {
-		return "", "", err
-	}
-	ed, err := time.Parse("20060102", end)
-	if err != nil {
-		return "", "", err
-	}
-	return sd.Format("2006-01-02"), ed.Format("2006-01-02"), nil
 }
 
 func getUserID(c *fiber.Ctx) string {
