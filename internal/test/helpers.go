e92c60497e14e0e77cc2ef5269e6aca8cd069ecb
Dylan Moreland <dylan@dimo.zone>
2023-04-27 19:08:05 -0400

    Get rid of giga mock

--- internal/test/helpers.go
+++ internal/test/helpers.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"time"
 
-	pb_devices "github.com/DIMO-Network/devices-api/pkg/grpc"
 	pb "github.com/DIMO-Network/users-api/pkg/grpc"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
@@ -16,7 +15,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func BuildRequest(method, url, body string) *http.Request {
@@ -109,44 +107,6 @@ type UsersClient struct {
 }
 
 func (c *UsersClient) GetUser(_ context.Context, in *pb.GetUserRequest, _ ...grpc.CallOption) (*pb.User, error) {
-	u, ok := c.Store[in.Id]
-	if !ok {
-		return nil, status.Error(codes.NotFound, "No user with that id found.")
-	}
-	return u, nil
-}
-
-type DevicesClient struct {
-	Store map[string]*pb_devices.UserDevice
-}
-
-// GetUserDevice(ctx context.Context, in *devices.GetUserDeviceRequest, opts ...grpc.CallOption) (*devices.UserDevice, error)
-func (c *DevicesClient) GetUserDeviceByTokenId(ctx context.Context, in *pb_devices.GetUserDeviceByTokenIdRequest, opts ...grpc.CallOption) (*pb_devices.UserDevice, error) {
-	return &pb_devices.UserDevice{}, nil
-}
-func (c *DevicesClient) ListUserDevicesForUser(ctx context.Context, in *pb_devices.ListUserDevicesForUserRequest, opts ...grpc.CallOption) (*pb_devices.ListUserDevicesForUserResponse, error) {
-	return &pb_devices.ListUserDevicesForUserResponse{}, nil
-}
-func (c *DevicesClient) ApplyHardwareTemplate(ctx context.Context, in *pb_devices.ApplyHardwareTemplateRequest, opts ...grpc.CallOption) (*pb_devices.ApplyHardwareTemplateResponse, error) {
-	return &pb_devices.ApplyHardwareTemplateResponse{}, nil
-}
-func (c *DevicesClient) GetAllUserDeviceValuation(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*pb_devices.ValuationResponse, error) {
-	return &pb_devices.ValuationResponse{}, nil
-}
-func (c *DevicesClient) GetUserDeviceByAutoPIUnitId(ctx context.Context, in *pb_devices.GetUserDeviceByAutoPIUnitIdRequest, opts ...grpc.CallOption) (*pb_devices.UserDeviceAutoPIUnitResponse, error) {
-	return &pb_devices.UserDeviceAutoPIUnitResponse{}, nil
-}
-func (c *DevicesClient) GetClaimedVehiclesGrowth(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*pb_devices.ClaimedVehiclesGrowth, error) {
-	return &pb_devices.ClaimedVehiclesGrowth{}, nil
-}
-func (c *DevicesClient) CreateTemplate(ctx context.Context, in *pb_devices.CreateTemplateRequest, opts ...grpc.CallOption) (*pb_devices.CreateTemplateResponse, error) {
-	return &pb_devices.CreateTemplateResponse{}, nil
-}
-func (c *DevicesClient) RegisterUserDeviceFromVIN(ctx context.Context, in *pb_devices.RegisterUserDeviceFromVINRequest, opts ...grpc.CallOption) (*pb_devices.RegisterUserDeviceFromVINResponse, error) {
-	return &pb_devices.RegisterUserDeviceFromVINResponse{}, nil
-}
-
-func (c *DevicesClient) GetUserDevice(_ context.Context, in *pb_devices.GetUserDeviceRequest, _ ...grpc.CallOption) (*pb_devices.UserDevice, error) {
 	u, ok := c.Store[in.Id]
 	if !ok {
 		return nil, status.Error(codes.NotFound, "No user with that id found.")
