d0e01be6346cab2c8d3d6a0f1f087c69a0ec67ba
James Reategui <jreate@me.com>
2023-06-23 06:43:32 -0400

    reorganize api

--- cmd/device-data-api/main.go
+++ cmd/device-data-api/main.go
@@ -8,9 +8,7 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"os/signal"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/DIMO-Network/device-data-api/internal/api"
@@ -24,29 +22,15 @@ import (
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/lovoo/goka"
 
-	"github.com/gofiber/fiber/v2/middleware/cache"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
 	_ "github.com/DIMO-Network/device-data-api/docs"
 	"github.com/DIMO-Network/device-data-api/internal/config"
-	"github.com/DIMO-Network/device-data-api/internal/controllers"
-	"github.com/DIMO-Network/device-data-api/internal/middleware/owner"
 	"github.com/DIMO-Network/device-data-api/internal/services"
 	dddatagrpc "github.com/DIMO-Network/device-data-api/pkg/grpc"
 	"github.com/DIMO-Network/shared"
-	"github.com/DIMO-Network/shared/middleware/privilegetoken"
-	pb "github.com/DIMO-Network/users-api/pkg/grpc"
-	"github.com/elastic/go-elasticsearch/v8"
-	"github.com/ethereum/go-ethereum/common"
-	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/swagger"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
+	"google.golang.org/grpc"
 )
 
 // @title                       DIMO Device Data API
@@ -109,117 +93,6 @@ func main() {
 		flag.Parse()
 		os.Exit(int(subcommands.Execute(ctx)))
 	}
-}
-
-func startWebAPI(logger zerolog.Logger, settings *config.Settings, dbs func() *db.ReaderWriter, definitionsAPIService services.DeviceDefinitionsAPIService, deviceAPIService services.DeviceAPIService) {
-	app := fiber.New(fiber.Config{
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			return ErrorHandler(c, err, logger)
-		},
-		DisableStartupMessage: true,
-		ReadBufferSize:        16000,
-	})
-
-	app.Use(metrics.HTTPMetricsMiddleware)
-
-	app.Use(recover.New(recover.Config{
-		Next:              nil,
-		EnableStackTrace:  true,
-		StackTraceHandler: nil,
-	}))
-	app.Use(cors.New())
-	//cache
-	cacheHandler := cache.New(cache.Config{
-		Next: func(c *fiber.Ctx) bool {
-			return c.Query("refresh") == "true"
-		},
-		Expiration:   2 * time.Minute,
-		CacheControl: true,
-	})
-
-	app.Get("/", healthCheck)
-	app.Get("/v1/swagger/*", swagger.HandlerDefault)
-
-	// secured paths
-	jwtAuth := jwtware.New(jwtware.Config{
-		JWKSetURLs: []string{settings.JwtKeySetURL},
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			return fiber.NewError(fiber.StatusUnauthorized, "Invalid JWT.")
-		},
-	})
-
-	esClient8, err := elasticsearch.NewTypedClient(elasticsearch.Config{
-		Addresses:  []string{settings.ElasticSearchAnalyticsHost},
-		Username:   settings.ElasticSearchAnalyticsUsername,
-		Password:   settings.ElasticSearchAnalyticsPassword,
-		MaxRetries: 5,
-	})
-	if err != nil {
-		logger.Fatal().Err(err).Msg("Error constructing Elasticsearch client.")
-	}
-
-	usersConn, err := grpc.Dial(settings.UsersAPIGRPCAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		logger.Fatal().Err(err).Msgf("Failed to dial users-api at %s", settings.UsersAPIGRPCAddr)
-	}
-	defer usersConn.Close()
-	usersClient := pb.NewUserServiceClient(usersConn)
-
-	deviceDataController := controllers.NewDeviceDataController(settings, &logger, deviceAPIService, esClient8, definitionsAPIService)
-
-	logger.Info().Str("jwkUrl", settings.TokenExchangeJWTKeySetURL).Str("vehicleAddr", settings.VehicleNFTAddress).Msg("Privileges enabled.")
-	privilegeAuth := jwtware.New(jwtware.Config{
-		JWKSetURLs: []string{settings.TokenExchangeJWTKeySetURL},
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			logger.Err(err).Msg("Privilege token error.")
-			return fiber.DefaultErrorHandler(c, err)
-		},
-	})
-
-	vToken := app.Group("/v1/vehicle/:tokenID", privilegeAuth)
-
-	tk := privilegetoken.New(privilegetoken.Config{
-		Log: &logger,
-	})
-	vehicleAddr := common.HexToAddress(settings.VehicleNFTAddress)
-
-	// Probably want constants for 1 and 4 here.
-	vToken.Get("/history", tk.OneOf(vehicleAddr, []int64{controllers.NonLocationData, controllers.AllTimeLocation}), deviceDataController.GetHistoricalRawPermissioned)
-
-	v1Auth := app.Group("/v1", jwtAuth)
-
-	udMw := owner.New(usersClient, deviceAPIService, &logger)
-	udOwner := v1Auth.Group("/user/device-data/:userDeviceID", udMw)
-	udOwner.Get("/historical", cacheHandler, deviceDataController.GetHistoricalRaw)
-	udOwner.Get("/distance-driven", cacheHandler, deviceDataController.GetDistanceDriven)
-	udOwner.Get("/daily-distance", cacheHandler, deviceDataController.GetDailyDistance)
-
-	dataDownloadController, err := controllers.NewDataDownloadController(settings, &logger, esClient8, deviceAPIService)
-	if err != nil {
-		panic(err)
-	}
-
-	udOwner.Post("/export/json/email", dataDownloadController.DataDownloadHandler)
-	go func() {
-		err = dataDownloadController.DataDownloadConsumer(context.Background())
-		if err != nil {
-			logger.Info().Err(err).Msg("data download consumer error")
-		}
-	}()
-
-	logger.Info().Msg("Server started on port " + settings.Port)
-	// Start Server from a different go routine
-	go func() {
-		if err := app.Listen(":" + settings.Port); err != nil {
-			logger.Fatal().Err(err)
-		}
-	}()
-	c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent with length of 1
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
-	<-c                                             // This blocks the main thread until an interrupt is received
-	logger.Info().Msg("Gracefully shutting down and running cleanup tasks...")
-	_ = app.Shutdown()
-	// shutdown anything else
 }
 
 func startGRPCServer(settings *config.Settings, dbs func() *db.ReaderWriter, logger *zerolog.Logger, definitionsAPIService services.DeviceDefinitionsAPIService) {
