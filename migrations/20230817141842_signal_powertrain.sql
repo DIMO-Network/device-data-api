6b929591535b7ac3122c3d768343de7634db4921
James Reategui <jreate@me.com>
2023-08-17 16:09:37 -0400

    add hybrid entries,set powertrains

--- migrations/20230817141842_signal_powertrain.sql
+++ migrations/20230817141842_signal_powertrain.sql
@@ -2,6 +2,35 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 SET search_path = device_data_api, public;
+alter table vehicle_signals_available_properties
+    alter column id type varchar(50) using id::varchar(50);
+
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'maf';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'runTime';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'engineLoad';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'intakeTemp';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'coolantTemp';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'engineSpeed';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'intakePressure';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'longTermFuelTrim1';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'shortTermFuelTrim1';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'fuelPercentRemaining';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ ICE, HEV, PHEV }' WHERE id LIKE 'oil';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ BEV, PHEV }' WHERE id LIKE 'soc';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ BEV, PHEV }' WHERE id LIKE 'chargeLimit';
+UPDATE vehicle_signals_available_properties SET power_train_type = '{ BEV, PHEV }' WHERE id LIKE 'charging';
+
+
+INSERT INTO vehicle_signals_available_properties(Id, name, power_train_type)
+VALUES ('hybridEVBatteryCapacity', 'hybridEVBatteryCapacity', '{ HEV, PHEV }'),
+       ('hybridEVBatteryChargeVoltage', 'hybridEVBatteryChargeVoltage', '{ HEV, PHEV }'),
+       ('hybridEVBatteryMaxCellVolrage', 'hybridEVBatteryMaxCellVolrage', '{ HEV, PHEV }'),
+       ('hybridEVBatteryRemainingChargeTime', 'hybridEVBatteryRemainingChargeTime', '{ HEV, PHEV }'),
+       ('hybridEVBatterySoh', 'hybridEVBatterySoh', '{ HEV, PHEV }'),
+       ('hybridEVBatteryRemaining', 'hybridEVBatteryRemaining', '{ HEV, PHEV }'),
+       ('hybridEVBatteryTemp', 'hybridEVBatteryTemp', '{ HEV, PHEV }'),
+       ('hybridEVEngineRPM', 'hybridEVEngineRPM', '{ HEV, PHEV }');
+
 
 -- +goose StatementEnd
 
