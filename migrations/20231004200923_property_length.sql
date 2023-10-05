ed2a36e55a7e57cad3dc750377dadafc8ee5ac81
James Reategui <jreate@me.com>
2023-10-04 20:13:13 -0400

    fix column length

---
+++ migrations/20231004200923_property_length.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+SET search_path = device_data_api, public;
+alter table report_vehicle_signals_events_all alter column property_id type varchar(50);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+SET search_path = device_data_api, public;
+alter table report_vehicle_signals_events_all alter column property_id type char(27);
+-- +goose StatementEnd
