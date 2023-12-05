a950750a0b3c3de41086107eaf61dbac1fc04a08
Allyson-English <allyson.english@gmail.com>
2023-12-04 19:20:42 -0700

    wip

--- internal/services/device_status_service.go
+++ internal/services/device_status_service.go
@@ -10,6 +10,7 @@ import (
 	"github.com/DIMO-Network/shared"
 	smartcar "github.com/smartcar/go-sdk"
 	"github.com/tidwall/gjson"
+	"github.com/volatiletech/null/v8"
 	"golang.org/x/exp/slices"
 )
 
@@ -153,6 +154,141 @@ func (dss *deviceStatusService) PrepareDeviceStatusInformation(ctx context.Conte
 	}
 
 	return ds
+}
+
+func (dss *deviceStatusService) PrepareDeviceStatusInformationV2(ctx context.Context, deviceData models.UserDeviceDatumSlice, deviceDefinitionID string, deviceStyleID *string, privilegeIDs []int64) response.Device {
+	deviceSnapshot := response.Device{}
+
+	// set the record created date to most recent one
+	for _, datum := range deviceData {
+		if deviceSnapshot.RecordCreatedAt == nil || deviceSnapshot.RecordCreatedAt.Unix() < datum.CreatedAt.Unix() {
+			deviceSnapshot.RecordCreatedAt = &datum.CreatedAt
+		}
+	}
+	// future: if time btw UpdateAt and timestamp > 7 days, ignore property
+
+	// todo further refactor by passing in type for each option, then have switch in function below, can also refactor timestamp thing
+	var status response.Status
+	if slices.Contains(privilegeIDs, NonLocationData) {
+		charging := findMostRecentSignal(deviceData, "charging", false)
+		if charging.Exists() {
+			c := charging.Get("value").Bool()
+			status.PowerTrain.TractionBattery.Charging.IsCharging = null.BoolFrom(c)
+		}
+		fuelPercentRemaining := findMostRecentSignal(deviceData, "fuelPercentRemaining", false)
+		if fuelPercentRemaining.Exists() {
+			ts := fuelPercentRemaining.Get("timestamp").Time()
+			if deviceSnapshot.RecordUpdatedAt == nil || deviceSnapshot.RecordUpdatedAt.Unix() < ts.Unix() {
+				deviceSnapshot.RecordUpdatedAt = &ts
+			}
+			f := fuelPercentRemaining.Get("value").Float()
+			if f >= 0.01 {
+				status.PowerTrain.FuelSystem.Level = null.Float64From(f)
+			}
+		}
+		batteryCapacity := findMostRecentSignal(deviceData, "batteryCapacity", false)
+		if batteryCapacity.Exists() {
+			b := batteryCapacity.Get("value").Float()
+			status.PowerTrain.TractionBattery.StateOfCharge.Current = null.Float64From(b)
+		}
+		oilLevel := findMostRecentSignal(deviceData, "oil", false)
+		if oilLevel.Exists() {
+			o := oilLevel.Get("value").Float()
+			if o > 0.75 {
+				status.PowerTrain.CombustionEngine.EngineOilLevel = null.StringFrom("CRITICALLY_HIGH")
+			} else if o >= 0.5 {
+				status.PowerTrain.CombustionEngine.EngineOilLevel = null.StringFrom("NORMAL")
+			} else if o >= 0.25 {
+				status.PowerTrain.CombustionEngine.EngineOilLevel = null.StringFrom("LOW_NORMAL")
+			} else if o > 0 {
+				status.PowerTrain.CombustionEngine.EngineOilLevel = null.StringFrom("CRITICALLY_LOW")
+			}
+
+		}
+		stateOfCharge := findMostRecentSignal(deviceData, "soc", false)
+		if stateOfCharge.Exists() {
+			o := stateOfCharge.Get("value").Float()
+			status.PowerTrain.TractionBattery.StateOfCharge.Displayed = null.Float64From(o)
+		}
+		chargeLimit := findMostRecentSignal(deviceData, "chargeLimit", false)
+		if chargeLimit.Exists() {
+			o := chargeLimit.Get("value").Float()
+			status.PowerTrain.TractionBattery.Charging.ChargeLimit = null.Float64From(o)
+		}
+		odometer := findMostRecentSignal(deviceData, "odometer", true)
+		if odometer.Exists() {
+			ts := odometer.Get("timestamp").Time()
+			if deviceSnapshot.RecordUpdatedAt == nil || deviceSnapshot.RecordUpdatedAt.Unix() < ts.Unix() {
+				deviceSnapshot.RecordUpdatedAt = &ts
+			}
+			o := odometer.Get("value").Float()
+			if shared.IsOdometerValid(o) {
+				status.TravelledDistance = null.Float64From(o)
+				status.PowerTrain.Transmission.TravelledDistance = null.Float64From(o)
+			}
+		}
+		rangeG := findMostRecentSignal(deviceData, "range", false)
+		if rangeG.Exists() {
+			r := rangeG.Get("value").Float()
+			status.PowerTrain.TractionBattery.Range = null.Float64From(r)
+		}
+		batteryVoltage := findMostRecentSignal(deviceData, "batteryVoltage", false)
+		if batteryVoltage.Exists() {
+			ts := batteryVoltage.Get("timestamp").Time()
+			if deviceSnapshot.RecordUpdatedAt == nil || deviceSnapshot.RecordUpdatedAt.Unix() < ts.Unix() {
+				deviceSnapshot.RecordUpdatedAt = &ts
+			}
+			bv := batteryVoltage.Get("value").Float()
+			status.LowVoltageBattery.CurrentVoltage = null.Float64From(bv)
+		}
+		ambientTemp := findMostRecentSignal(deviceData, "ambientTemp", false)
+		if ambientTemp.Exists() {
+			at := ambientTemp.Get("value").Float()
+			status.Exterior.AirTemperature = null.Float64From(at)
+		}
+		// TirePressure
+		tires := findMostRecentSignal(deviceData, "tires", false)
+		if tires.Exists() {
+			// weird thing here is in example payloads these are all ints, but the smartcar lib has as floats
+			fl := tires.Get("value").Get("frontLeft").Float()
+			fr := tires.Get("value").Get("frontRight").Float()
+			bl := tires.Get("value").Get("backLeft").Float()
+			br := tires.Get("value").Get("backRight").Float()
+			status.Chasis.Axle.Row1.Wheel.Left.Tire.Pressure = null.Float64From(fl)
+			status.Chasis.Axle.Row1.Wheel.Right.Tire.Pressure = null.Float64From(fr)
+			status.Chasis.Axle.Row2.Wheel.Left.Tire.Pressure = null.Float64From(bl)
+			status.Chasis.Axle.Row2.Wheel.Right.Tire.Pressure = null.Float64From(br)
+
+		}
+	}
+
+	if slices.Contains(privilegeIDs, CurrentLocation) || slices.Contains(privilegeIDs, AllTimeLocation) {
+		latitude := findMostRecentSignal(deviceData, "latitude", false)
+		if latitude.Exists() {
+			ts := latitude.Get("timestamp").Time()
+			if deviceSnapshot.RecordUpdatedAt == nil || deviceSnapshot.RecordUpdatedAt.Unix() < ts.Unix() {
+				deviceSnapshot.RecordUpdatedAt = &ts
+			}
+			l := latitude.Get("value").Float()
+			status.CurrentLocation.Latitude = null.Float64From(l)
+		}
+		longitude := findMostRecentSignal(deviceData, "longitude", false)
+		if longitude.Exists() {
+			l := longitude.Get("value").Float()
+			status.CurrentLocation.Longitude = null.Float64From(l)
+		}
+	}
+
+	if status.PowerTrain.TractionBattery.Range.IsZero() && !status.PowerTrain.FuelSystem.Level.IsZero() {
+		calcRange, err := dss.CalculateRange(ctx, deviceDefinitionID, deviceStyleID, status.PowerTrain.FuelSystem.Level.Float64)
+		if err == nil {
+			status.PowerTrain.TractionBattery.Range = null.Float64From(*calcRange)
+		}
+	}
+
+	deviceSnapshot.Status = append(deviceSnapshot.Status, status)
+
+	return deviceSnapshot
 }
 
 // findMostRecentSignal finds the highest value float instead of most recent, eg. for odometer
